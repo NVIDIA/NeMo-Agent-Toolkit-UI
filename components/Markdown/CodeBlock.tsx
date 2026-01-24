a8083710780a88005b161cf95460245a2308b4a0
Will Killian <2007799+willkill07@users.noreply.github.com>
2026-01-23 19:35:33 -0500

    fix: ensure intermediate step details have maximum width (#86)
    - clamp width of intermediate step details
    - clamp width of main window
    - ensure chat messages are scrollable
    
    Closes
    
    ## By Submitting this PR I confirm:
    - I am familiar with the [Contributing Guidelines](https://github.com/NVIDIA/NeMo-Agent-Toolkit-UI/blob/main/CODE-OF-CONDUCT.md).
    - We require that all contributors "sign-off" on their commits. This certifies that the contribution is your original work, or you have rights to submit it under the same license, or a compatible license.
      - Any contribution which contains commits that are not Signed-Off will not be accepted.
    - When the PR is ready for review, new or existing tests cover these changes.
    - When the PR is ready for review, the documentation is up to date with these changes.
    
    Authors:
      - Will Killian (https://github.com/willkill07)
    
    Approvers:
      - Eric Evans II (https://github.com/ericevans-nv)
      - David Gardner (https://github.com/dagardner-nv)
    
    URL: https://github.com/NVIDIA/NeMo-Agent-Toolkit-UI/pull/86

--- components/Markdown/CodeBlock.tsx
+++ components/Markdown/CodeBlock.tsx
@@ -103,8 +103,7 @@ export const CodeBlock: FC<Props> = memo(({ language, value }) => {
         style={oneDark}
         customStyle={{
           margin: 0,
-          // width: 'max-content',
-          maxWidth: '200ch',
+          maxWidth: '100%',
           maxHeight: '50vh',
           display: 'block',
           boxSizing: 'border-box',
